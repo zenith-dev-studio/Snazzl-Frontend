9e482138815f6429fb9a2d0126da35346ed33be5
Shresth Pratyush <shresthpratyush@Shresths-MacBook-Air.local>
2025-10-03 23:41:58 +0530

    Removed clerk for now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 
 import ProductDashboard from "./pages/Products";
 import ProductAdd from "./pages/ProductAdd";
@@ -18,112 +17,27 @@ import AdminDelivery from "./pages/AdminDelivery";
 import AdminProducts from "./pages/AdminProducts";
 import AdminStoreAdd from "./pages/AdminStoreAdd";
 
-
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/shop/products"
-          element={
-              <ProductDashboard />
-          }
-        />
+        <Route path="/shop/products" element={<ProductDashboard />} />
+        <Route path="/shop/orders" element={<OrdersPage />} />
+        <Route path="/shop/customers" element={<CustomersTable />} />
+        <Route path="/shop/profile" element={<EditProfile />} />
+        <Route path="/shop/notifications" element={<NotificationsPage />} />
+        <Route path="/shop/settings" element={<AccountPage />} />
+        <Route path="/shop/" element={<Overview />} />
+        <Route path="/shop/products/add" element={<ProductAdd />} />
 
-        <Route
-  path="/shop/orders"
-  element={
-       <OrdersPage/>
-  }
-/>
-      <Route
-  path="/shop/customers"
-  element={
-        <CustomersTable/>
-  }
-/>
-<Route
-  path="/shop/profile"
-  element={
-    <EditProfile/>
-  }
-/>
-<Route
-  path="/shop/notifications"
-  element={
-    <NotificationsPage/>
-  }
-/>
-<Route
-  path="/shop/settings"
-  element={
-    <AccountPage/>
-  }
-/>
-<Route
-  path="/shop/"
-  element={
-    <Overview/>
-  }
-/>
-<Route
-  path="/shop/products/add"
-  element={
-    <ProductAdd/>
-  }
-/>
-<Route
-  path="/admin/"
-  element={
-    <AdminDashboard/>
-  }
-/>
-<Route
-  path="/admin/orders"
-  element={
-    <AdminOrder/>
-  }
-/>
-<Route
-  path="/admin/order"
-  element={
-    <AdminOrderPage/>
-  }
-/>
-<Route
-  path="/admin/store"
-  element={
-    <AdminStore/>
-  }
-/>
-<Route
-  path="/admin/customers"
-  element={
-    <AdminCustomers/>
-  }
-/>
-<Route
-  path="/admin/delivery"
-  element={
-    <AdminDelivery/>
-  }
-/>
-<Route
-  path="/admin/products"
-  element={
-    <AdminProducts/>
-  }
-/>
-       <Route
-  path="/shop/products"
-  element={<ProductDashboard/>}
-/>
-<Route
-  path="/admin/store/add"
-  element={
-    <AdminStoreAdd/>
-  }
-/>
+        <Route path="/admin/" element={<AdminDashboard />} />
+        <Route path="/admin/orders" element={<AdminOrder />} />
+        <Route path="/admin/order" element={<AdminOrderPage />} />
+        <Route path="/admin/store" element={<AdminStore />} />
+        <Route path="/admin/customers" element={<AdminCustomers />} />
+        <Route path="/admin/delivery" element={<AdminDelivery />} />
+        <Route path="/admin/products" element={<AdminProducts />} />
+        <Route path="/admin/store/add" element={<AdminStoreAdd />} />
       </Routes>
     </Router>
   );
